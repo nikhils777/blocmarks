a1da937588439f893176b02829a3aad461b73378
nikhils777 <nsharmatennis@gmail.com>
2014-07-14 17:22:10 -0400

    minor update

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,5 +1,5 @@
 class IncomingController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: [:create]
+  skip_before_filter :verify_authenticity_token
   def create
     puts "INCOMING PARAMS HERE: #{params}"
     
