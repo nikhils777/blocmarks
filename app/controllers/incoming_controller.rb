9ad5a7bbecb206c24f0c513951912c1a655b0b81
nikhils777 <nsharmatennis@gmail.com>
2014-07-14 18:45:59 -0400

    update creating blocmarks

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,7 +2,11 @@ class IncomingController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def create
     puts "INCOMING PARAMS HERE: #{params}"
-    
-    head 200
+    topic = params[subject]
+    user = params[sender]
+    text = params[body-plain]
+    User.all.where(email: user )
+    x = Topic.new(user_id: User.all.where(email: user )).save
+    Blocmark.new(topic_id: x.id , user_id: User.all.where(email: user )).save
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714160407) do
+ActiveRecord::Schema.define(version: 20140714184421) do
+
+  create_table "blocmarks", force: true do |t|
+    t.string   "url"
+    t.integer  "user_id"
+    t.integer  "topic_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blocmarks", ["topic_id"], name: "index_blocmarks_on_topic_id"
+  add_index "blocmarks", ["user_id"], name: "index_blocmarks_on_user_id"
+
+  create_table "topics", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "topics", ["user_id"], name: "index_topics_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
