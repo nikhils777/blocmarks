8e3947d8ad015c3f654d7ae02386cac7264b6081
nikhils777 <nsharmatennis@gmail.com>
2014-07-16 17:18:40 -0400

    update

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -7,7 +7,7 @@ class IncomingController < ApplicationController
     
     user = User.all.where(email: sender )
     if user
-      new_topic = Topic.find_or_create_by(title: params["subject"])
+      new_topic = Topic.all.find_or_create_by(title: params["subject"])
       Blocmark.find_or_create_by(topic_id: new_topic.id , user_id: user.first.id, url: params["body-plain"])
     else
       puts "EEEEERRRRROOOORRRROOOORRRR"
