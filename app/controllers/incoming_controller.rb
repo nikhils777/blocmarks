1a5b1a011c6f16ef3d275acbb769663583d809ad
nikhils777 <nsharmatennis@gmail.com>
2014-07-16 18:42:12 -0400

    update

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,17 +1,13 @@
 class IncomingController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def create
-    puts "IIIINNNNNNNNCCCOOOOOOMMMMIIINNNNNGGGG!!! #{params}"
-
     sender = params[:sender]
     
-    user = User.all.where(email: sender )
+    user = User.where(email: sender)
     if user.exists?
-      logger.info "Got inside the if block"
-      new_topic = Topic.create(title: params[:subject])
-      Blocmark.create(topic_id: new_topic.id , user_id: user.first.id, url: params[:'body-plain'])
-    else
-      puts "EEEEERRRRROOOORRRROOOORRRR"
+      new_topic = Topic.create(:title => sender)
+      Blocmark.create(topic_id: new_topic.id , user_id: user.first.id, url: params[:'body-plain'])
+      head 200
     end
   end
 end
