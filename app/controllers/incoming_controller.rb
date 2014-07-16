203fca2fdc47baa06d2661884aa414ed975100d2
nikhils777 <nsharmatennis@gmail.com>
2014-07-16 18:51:06 -0400

    update

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,11 +1,11 @@
 class IncomingController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def create
-    sender = params[:sender]
+    subject = params[:subject]
     
     user = User.where(email: sender)
     if user.exists?
-      new_topic = Topic.create(:title => sender)
+      new_topic = Topic.create(:title => subject)
       Blocmark.create(topic_id: new_topic.id , user_id: user.first.id, url: params[:'body-plain'])
       head 200
     end
