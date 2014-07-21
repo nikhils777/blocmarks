2a4040398a09089c5fa392cfa957ded486d36cbf
nikhils777 <nsharmatennis@gmail.com>
2014-07-21 19:26:27 -0400

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @topics = current_user.topics
 
-    @topics_hash = current_user.blocmarks.group_by{ |blocmark| blockmark.topic }
+    @topics_hash = current_user.blocmarks.group_by{ |blocmark| blocmark.topic }
   end
 
   def about
